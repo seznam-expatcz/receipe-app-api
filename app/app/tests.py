5d9aeed43be6a15a9163da38b224bbe8d98a836f
expat cz <expatcz@seznam.cz>
2022-08-13 11:17:57 +0200

    07-28 - Write a test using TDD

--- app/app/tests.py
+++ app/app/tests.py
@@ -14,3 +14,9 @@ class CalcTest(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
+
+    def test_subtract_number(self):
+        """ Test subtracting numbers """
+        res = calc.subtract(7, 4)
+
+        self.assertEqual(res, 3)
